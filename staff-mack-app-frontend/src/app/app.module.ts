4d01a27c374b251cdb5712f7d35d71e419625d3f
Gabrielly Mendes <silvamgabi@hotmail.com>
2024-03-27 00:37:52 -0300

    feat: adicionado página inicial presence control

--- staff-mack-app-frontend/src/app/app.module.ts
+++ staff-mack-app-frontend/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavigationMenuComponent } from './core/navigation-menu/navigation-menu.component';
 import { ReportPageComponent } from './pages/report-page/report-page.component';
 import { PresenceControlPageComponent } from './pages/presence-control-page/presence-control-page.component';
+import { FilterComponent } from './shared/components/filter/filter.component';
+import { SearchComponent } from './shared/components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PresenceControlPageComponent,
     ReportPageComponent,
-    NavigationMenuComponent
+    NavigationMenuComponent,
+    FilterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
